8d2e6e57416fcd4099262fcb852c301937a67bac
Nicholas Iannone (nickthedude) <nickthedude@me.com>
2012-03-05 12:01:32 -0800

    getting it working slightly

---
+++ gitSuggest/TMGitSuggestEngine.h
@@ -0,0 +1,13 @@
+//
+//  TMGitSuggestEngine.h
+//  gitSuggest
+//
+//  Created by Nicholas Iannone on 3/5/12.
+//  Copyright (c) 2012 Tiny Mobile Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMGitSuggestEngine : NSObject
+
+@end
---
+++ gitSuggest/TMGitSuggestEngine.m
@@ -0,0 +1,13 @@
+//
+//  TMGitSuggestEngine.m
+//  gitSuggest
+//
+//  Created by Nicholas Iannone on 3/5/12.
+//  Copyright (c) 2012 Tiny Mobile Inc. All rights reserved.
+//
+
+#import "TMGitSuggestEngine.h"
+
+@implementation TMGitSuggestEngine
+
+@end
